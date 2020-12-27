eab2ba63808f0ab7d8a958aa1ad088aa7a1fa732
Madison <madisonakrell@gmail.com>
2020-12-27 16:22:28 -0600

    ToDo: fix verification

---
+++ day4.py
@@ -0,0 +1,25 @@
+file = open("test.txt", 'r')
+check = True
+count = 0
+while check:
+    pp = ""
+    line = file.readline()
+    if not line:
+        check = False
+    else:
+        nlines = 0 
+        pp = pp + line
+        while nlines < 1 and line:
+            line = file.readline()
+            if line == '\n':
+                nlines = nlines + 1
+            else:
+                pp = pp + line
+        #if pp.find("byr") == 1 and pp.find("iyr") == 1 and pp.find("eyr") == 1 and pp.find("hgt") == 1 and pp.find("hcl") == 1 and pp.find("ecl") == 1 and pp.find("pid") == 1:
+        #not working here
+        if not pp.find('hgt'):
+            count = count + 1
+print(count)
+
+
+file.close()
