ddcbfc1c04110d2f9e18b54b73244d8a50ef779e
Madison <madisonakrell@gmail.com>
2020-12-27 10:48:25 -0600

    part 2 completed

--- day1.py
+++ day1.py
@@ -1,16 +1,23 @@
-#part 1 done
 entry = []
+found = []
 file = open('input.txt', 'r')
 check = True
+#to read in all inputs from file
 while check:
     input = file.readline()
     if not input:
         check = False
     else:
         entry.append(int(input))
-print("hello")
 for x in entry:
     temp = 2020-x
-    if temp in entry:
-        print(x*temp)
-print("this")
+    for y in entry:
+        if y != x:
+            temp2 = temp - y
+            if temp2 in entry:
+                #to make sure no double answers are found
+                if x not in found and y not in found and temp2 not in found:
+                    found.append(x)
+                    found.append(y)
+                    found.append(temp2)
+                    print(x*y*temp2)
